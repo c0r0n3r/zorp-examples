561211f393097819a966ff4ec77aae9aeba174ca
Szilárd Pfeiffer <szilard.pfeiffer@balabit.com>
2011-06-26 15:08:17 +0200

    Initial commit.

---
+++ policy.py
@@ -0,0 +1,23 @@
+#############################################################################
+## -*- coding: UTF-8 -*-
+##
+## Copyright (c) 2011 BalaBit IT Ltd, Budapest, Hungary
+## Copyright (c) 2011 Szilárd Pfeiffer <szilard.pfeiffer@balabit.com>
+## Copyright (c) 2011 Tibor Balázs <tibor.balazs@balabit.com>
+##
+## Authors: Szilárd Pfeiffer <szilard.pfeiffer@balabit.com>
+##          Tibor Balázs <tibor.balazs@balabit.com>
+##
+## Permission is granted to copy, distribute and/or modify this document
+## under the terms of the GNU Free Documentation License, Version 1.3
+## or any later version published by the Free Software Foundation;
+## with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+## A copy of the license is included in the section entitled "GNU
+## Free Documentation License".
+##
+#############################################################################
+
+from Zorp.Core import *
+
+def zorp_instance():
+	pass
